4def762584d11a536c5fe7a72d319f8948bfd93d
torbjoau <torbjoau@stud.ntnu.no>
2022-03-11 00:11:06 +0100

    postgresql and bolig model

--- Gib2Prosjekt/models.py
+++ Gib2Prosjekt/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Bolig(models.Model):
+    address = models.CharField(max_length=100)
+    desc = models.TextField
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
+
+
+
