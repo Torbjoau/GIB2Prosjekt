fe0b78f14223b07abaf837799615eda74ada9538
kirst <kirsteru@stud.ntnu.no>
2022-04-12 19:00:42 +0200

    lagt til data

--- Gib2Prosjekt/views.py
+++ Gib2Prosjekt/views.py
@@ -14,18 +14,14 @@ def hjem(request):
     return render(request, 'Bolig.html',
         {'list_bolig': list_bolig})
 
+print('hello')
 
 def bolig_view(request, slug):
     bolig_view = get_object_or_404(Bolig, slug=slug)
     return render(request, 'bolig_detail.html', {'bolig_view' : bolig_view})
 
 geolocator=Nominatim(user_agent="my_request")
-v="Klostergata 18B,Trondheim,Norway"
-print(v)
-location=geolocator.geocode(v)
-print((location.longitude, location.latitude))
-l=geolocator.geocode("Roseveien 2,Lier,Norway")
-p=geolocator.geocode("Kong Inges gate 3,Trondheim,Norway")
+
 
 #hus = Bolig.objects.all()
 #for i in hus:
@@ -34,10 +30,10 @@ p=geolocator.geocode("Kong Inges gate 3,Trondheim,Norway")
 
 def kart(request):
     m = folium.Map(location=[63.417190066978264, 10.404224395751953], zoom_start=12)
-    folium.Marker(location=[63.417190066978264, 10.404224395751953]).add_to(m)
-    folium.Marker(location=[location.latitude,location.longitude]).add_to(m)
-    folium.Marker(location=[l.latitude,l.longitude]).add_to(m)
-    folium.Marker(location=[p.latitude,p.longitude]).add_to(m)
+    #folium.Marker(location=[63.417190066978264, 10.404224395751953]).add_to(m)
+    #folium.Marker(location=[location.latitude,location.longitude]).add_to(m)
+    #folium.Marker(location=[l.latitude,l.longitude]).add_to(m)
+    #folium.Marker(location=[p.latitude,p.longitude]).add_to(m)
     hus=Bolig.objects.all()
     for i in hus:
         g=geolocator.geocode(i.address)
@@ -46,7 +42,8 @@ def kart(request):
         iframe = folium.IFrame(html)
         Popup=folium.Popup(iframe, min_width=200, max_width=800)
 
-        folium.Marker(location=[g.latitude, g.longitude], popup=Popup, icon=folium.Icon(color='lightgray', icon='home')).add_to(m)
+        folium.Marker(location=[g.latitude, g.longitude], popup=Popup, icon=folium.Icon(color='red', icon='home')).add_to(m)
+
 
     m = m._repr_html_()
     context = {
