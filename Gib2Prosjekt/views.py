9b0d97e0bd2182c8a3e94b1ce44c47a61eea3827
kriegg <kriegg@stud.ntnu.no>
2022-04-26 15:39:47 +0200

    testt

--- Gib2Prosjekt/views.py
+++ Gib2Prosjekt/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, get_object_or_404, redirect
 import folium
+import numpy as np
 from geopy.geocoders import Nominatim
 from .models import Bolig
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
 from .models import BoligForm
+import base64
+import cgi
 
 # Create your views here.
 
@@ -63,28 +66,176 @@ def kart(request):
 
 
 def kart(request):
-    m = folium.Map(location=[63.417190066978264, 10.404224395751953], zoom_start=12, hight=600, width=1800)
+    form_inputs = cgi.FieldStorage()
+    m = folium.Map(location=[63.417190066978264, 10.404224395751953], zoom_start=12, hight=600, width=900)
     if request.method == 'POST':
         list = []
         type = request.POST.get("type")
-        for i in Bolig.objects.all():
-            if i.type == type:
-                list.append(i)
-        #return redirect('kart')
+        price = request.POST.get("price")
+        type = request.POST.get("type")
+        area = request.POST.get("area")
+        bedroom = request.POST.get("bedroom")
+        energy = request.POST.get("energy")
+        year = request.POST.get("year")
 
+        price1 = request.POST.get("price_telle")
+        type1 = request.POST.get("type")
+        area1 = request.POST.get("area_telle")
+        bedroom1 = request.POST.get("bedroom_telle")
+        energy1 = request.POST.get("energy_telle")
+        year1 = request.POST.get("year_telle")
+
+        price_list = []
+        for i in Bolig.objects.all():
+            price_list.append(i.price)
+        tot_max_price = max(price_list)
+        tot_min_price = min(price_list)
+        print("maks pris: ", tot_max_price, " min pris: ", tot_min_price)
+        for i in Bolig.objects.all():
+            if ((i.price < float(price or 0) or form_inputs.getvalue("price") is None )) and (i.area > int(area or 0)) and (i.bedroom >= int(bedroom or 0)) and ((i.type == type)) and (energy != "true" or (ord(i.energy) <= ord(energy))) and (i.year >= int(year or 0)):
+                list.append(i)
+
+        #return redirect('kart')
+        print(list)
+        print(float(price or 0))
+        print(int(year or 0))
+        print(type)
         #hus=Bolig.objects.all()
+
+        max_points = 0
+        min_points = 99999999999
+        list_points = []
         for i in list:
-            g=geolocator.geocode(i.address+",Trondheim,Norway")
-            html = folium.Html('<a href="http://127.0.0.1:8000/Bolig/' + i.slug + '" target="_blank">' + i.address + '</a>', script=True)
-            #html = '<a href="../Bolig/%s"> test </a>'%i.slug
-            iframe = folium.IFrame(html)
-            Popup=folium.Popup(iframe, min_width=200, max_width=800)
-            if int(i.price) <= 2000000:
-                folium.Marker(location=[g.latitude, g.longitude], popup=Popup, tooltip="trykk for mer infromasjon", icon=folium.Icon(color='lightred', icon='home')).add_to(m)
-            elif 2000000 < int(i.price) <= 8000000:
-                folium.Marker(location=[g.latitude, g.longitude], popup=Popup, tooltip="trykk for mer infromasjon", icon=folium.Icon(color='red', icon='home')).add_to(m)
+            print("NY BOLIG")
+            points = 0
+            #Dersom energiklasse er valgt, men ikke huket av for høy energiklasse
+            if energy1 != "true" and energy != "true":
+                print("alt1_energi")
+                if i.energy == 'A' or i.energy == 'B':
+                    points += 3
+                elif i.energy == 'C' or i.energy == 'D':
+                    points += 2
+                else:
+                    points += 1
+            #Derson energiklasse er valgt og huket av for høy energiklasse eller kun huket av for høy energiklasse
+            if (energy1 == "true" and energy != "true") or (energy1 == "true" and energy == "true"):
+                if i.energy == 'A' or i.energy == 'B':
+                    points += 3*2
+                elif i.energy == 'C' or i.energy == 'D':
+                    points += 2*2
+                else:
+                    points += 1*2
+            print("Poeng energi: ", points)
+
+            if int(bedroom or 0) == 0:
+                points += 0
             else:
-                folium.Marker(location=[g.latitude, g.longitude], popup=Popup, tooltip="trykk for mer infromasjon", icon=folium.Icon(color='darkred', icon_color="gray", icon='home')).add_to(m)
+                if i.bedroom == int(bedroom):
+                    pointsBedroom = 3
+                elif i.bedroom == (int(bedroom)+1):
+                    pointsBedroom = 2
+                else:
+                    pointsBedroom = 1
+                if bedroom1 == "true":
+                    points += 2*pointsBedroom
+                else:
+                    points += pointsBedroom
+
+            #Dersom byggeår er gitt og ikke ønsker nytt hus
+            if int(year or 0) != 0 and year1 != "true":
+                print("alt1_bygg")
+                if i.year >= int(year or 0)+15:
+                    points += 3
+                elif int(year)+10 <= i.year < int(year or 0)+15:
+                    points += 2
+                else:
+                    points += 1
+            #Dersom byggeår er gitt og ønsker nytt hus = legg til vekting
+            if int(year or 0) != 0 and year1 == "true":
+                print("alt2_bygg")
+                if i.year >= int(year or 0)+15:
+                    points += 3*2
+                elif int(year)+10 <= i.year < int(year or 0)+15:
+                    points += 2*2
+                else:
+                    points += 1*2
+            #Dersom byggeår ikke er gitt, men ønsker nytt hus = bruk gitte intervaller
+            if int(year or 0) == 0 and year1 == "true":
+                print("alt3_bygg")
+                if i.year >= 2010:
+                    points += 3
+                elif 1990 < int(i.year) <= 1990:
+                    points += 2
+                else:
+                    points += 1
+
+            print("poeng før pris: ", points)
+            #Dersom makspris er fylt ut og ikke ønsker lav pris = flest poeng til boliger nærmest makspris
+            if float(price or 0) != 0 and price1 != "true":
+                print("alt1")
+                if i.price >= 3*int(price)/4:
+                    points += 3
+                elif i.price >= int(price)/2:
+                    points += 2
+                else:
+                    points += 1
+            #Dersom makspris er fylt ut, men ønsker lav pris = flest poeng til bolig med lavest pris
+            elif float(price or 0) != 0 and price1 == "true":
+                print("alt2")
+                if i.price <= int(price)/3:
+                    points += 3
+                elif i.price <= 2*int(price)/3:
+                    points += 2
+                else:
+                    points += 1
+            #Dersom makspris ikke er fylt ut, men ønsker lav pris = flest poeng til bolig med lavest pris (ser på hele databasen)
+            elif float(price or 0) == 0 and price1 == "true":
+                print("alt3")
+                if i.price <= tot_min_price + (tot_max_price-tot_min_price)/3:
+                    points += 3
+                elif i.price <= tot_min_price + (2*(tot_max_price-tot_min_price))/3:
+                    points += 2
+                else:
+                    points += 1
+            print("poeng etter pris: ", points)
+            list_points.append(points)
+            if points > max_points:
+                max_points = points
+            if points < min_points:
+                min_points = points
+        print("maks: ", max_points, " min: ", min_points)
+        print("darkred:", min_points + 3*(max_points-min_points)/4, " red:", min_points + (max_points-min_points)/2, " lightred:", min_points + (max_points - min_points)/4)
+
+        i = 0
+        for item in list:
+            g=geolocator.geocode(item.address+",Trondheim,Norway")
+            #html = folium.Html('<a href="http://127.0.0.1:8000/Bolig/' + item.slug + '" target="_blank">' + item.address + '</a>', "<img scr="+str(item.image)+"/>")#, script=True)
+
+
+            encoded = base64.b64encode(open("media/" + str(item.image), 'rb').read())
+            # html = '<img src="data:image/jpeg;base64,{}" width=250 height=250>'.format
+            html = f'''
+
+                    <h1 style="font-family: 'Century Gothic'"> {item.address} </h1>
+                    <a href="http://127.0.0.1:8000/Bolig/{item.slug}" target="_blank"> <img src="data:image/jpeg;base64,{{}}" height=250> </a>
+
+                    '''.format
+            iframe = folium.IFrame(html(encoded.decode('UTF-8')), width=400, height=350)
+
+            #iframe = folium.IFrame(html)
+            print(item.image)
+            Popup=folium.Popup(iframe, min_width=200, max_width=800)
+
+            if list_points[i] >= min_points + 3*(max_points-min_points)/4:
+                folium.Marker(location=[g.latitude, g.longitude], popup=Popup, tooltip="trykk for mer informasjon", icon=folium.Icon(color='darkred', icon='home')).add_to(m)
+            elif min_points + (max_points-min_points)/2 <= list_points[i] < min_points + 3*(max_points-min_points)/4:
+                folium.Marker(location=[g.latitude, g.longitude], popup=Popup, tooltip="trykk for mer informasjon", icon=folium.Icon(color='red', icon='home')).add_to(m)
+            elif min_points + (max_points - min_points)/4 <= list_points[i] < min_points + (max_points - min_points)/2:
+                folium.Marker(location=[g.latitude, g.longitude], popup=Popup, tooltip="trykk for mer informasjon", icon=folium.Icon(color='lightred', icon='home')).add_to(m)
+            else:
+                folium.Marker(location=[g.latitude, g.longitude], popup=Popup, tooltip="trykk for mer informasjon", icon=folium.Icon(color='white', icon='home')).add_to(m)
+            
+            i += 1
 
         m = m._repr_html_()
         context = {
@@ -189,25 +340,4 @@ def delete2(request, slug):
     bolig_update = get_object_or_404(Bolig, slug=slug)
     bolig_update.delete()
     return redirect('Bolig')
-
-def valg(request):
-    if request.method == 'POST':
-        price = request.POST.get("price")
-        type = request.POST.get("type")
-        area = request.POST.get("area")
-        bedrooms = request.POST.get("bedrooms")
-        energy = request.POST.get("energy")
-        print(price, type, area, bedrooms, energy)
-        l=[]
-        for i in Bolig.objects.all():
-
-            if (i.price < float(price or 0)) and (i.area > int(area or 0)) and (i.bedroom >= int(bedrooms or 0)) and (i.type == type) and (ord(i.energy) <= ord(energy)):
-            #if i.type == type:
-                l.append(i)
-        print(l)
-        return redirect('kart')
-    else:
-        return render(request, 'valg.html')
-
-
 
